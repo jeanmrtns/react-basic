71ca70c24510f249756782d5a05c3b56088bfffe
Jean <jeanmrtns4@gmail.com>
2020-11-07 15:15:44 -0300

    Adicionando Header

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../styles/Header.css'
+
+const Header = () => (
+    <header>
+        <h1>Olá, mundo!</h1>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </header>
+);
+
+export default Header
