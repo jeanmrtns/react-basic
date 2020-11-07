5b3305f3fac83fbc1aed6fe5f366fba1d180c6fc
Jean <jeanmrtns4@gmail.com>
2020-11-07 16:56:43 -0300

    add button adicionar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-import Header from './components/Header'
-import Comentario from './components/Comentarios';
+import Header from "./components/Header";
+import Comentario from "./components/Comentarios";
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Comentario nome="Jean" email="jeanmrtns4@gmail.com" data={ new Date(24,3,1999) }>
-        Olá, mundo!
-      </Comentario>
-      <Comentario nome="Maria" email="maria@gmail.com" data={new Date(2,5,2020)} >
-        Nem demorei
-      </Comentario>
-    </div>
-  );
+class App extends Component {
+  state = {
+    comentarios: [
+      {
+        nome: "Jean",
+        email: "jeanmrtns4@gmail.com",
+        data: new Date(2020, 11, 7),
+        mensagem: "Olá, mundo!",
+      },
+      {
+        nome: "Maria",
+        email: "maria@mail.com",
+        data: new Date(2020, 11, 6),
+        mensagem: "Hello, darkness",
+      },
+    ],
+  };
+
+  adicionarComentario = () => {
+    console.log("oi");
+
+    const novoComentario = {
+      nome: "Hey",
+      email: "hey@mail.com",
+      data: new Date(),
+      mensagem: "Hello",
+    };
+
+    this.setState({
+      comentarios: [ ...this.state.comentarios, novoComentario ]
+    });
+    
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <h1>Meu projeto</h1>
+
+        {this.state.comentarios.map((comentario, index) => (
+          <Comentario
+            key={index}
+            nome={comentario.nome}
+            email={comentario.email}
+            data={comentario.data.toString()}
+          >
+            {comentario.mensagem}
+          </Comentario>
+        ))}
+
+        <button className="addComentario" onClick={this.adicionarComentario}>
+          Adicionar comentário
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
